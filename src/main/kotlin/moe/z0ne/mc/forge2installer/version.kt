cb570040cf5cee63b0754addc7015ce66fbac865
Christopher König <iam@z0ne.moe>
2020-03-09 22:55:45 +0100

    fixed rootProject name

--- src/main/kotlin/moe/z0ne/mc/forge2installer/version.kt
+++ src/main/kotlin/moe/z0ne/mc/forge2installer/version.kt
@@ -5,10 +5,10 @@ package moe.z0ne.mc.forge2installer
  */
 const val MAVEN_GROUP = "moe.z0ne.mc"
 const val MAVEN_NAME = "forge1"
-const val VERSION = "1.1"
-const val GIT_REVISION = 3
-const val GIT_SHA = "8b11a6c717ffaf3d541664a75fa68dd7e98bfb41"
-const val GIT_DATE = "2020-03-05T12:17:08Z"
-const val BUILD_DATE = "2020-03-05T12:17:25Z"
-const val BUILD_UNIX_TIME = 1583410645852L
-const val DIRTY = 0
+const val VERSION = "1.0"
+const val GIT_REVISION = 1
+const val GIT_SHA = "ddb2b74b6c0f860aa3aba20c3396f2bab48be9ca"
+const val GIT_DATE = "2020-03-09T21:52:44Z"
+const val BUILD_DATE = "2020-03-09T21:54:25Z"
+const val BUILD_UNIX_TIME = 1583790865764L
+const val DIRTY = 1
